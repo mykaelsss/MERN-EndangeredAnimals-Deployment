a8bf941123a28ffba58bb29024bfa2959569e296
mykaelsss <112145455+mykaelsss@users.noreply.github.com>
2022-11-16 20:11:40 -0500

    Update MobileNavigation.jsx

--- mern-project/client/src/components/MobileNavigation.jsx
+++ mern-project/client/src/components/MobileNavigation.jsx
@@ -77,7 +77,7 @@ const MobileNavigation = ({check,change}) => {
         <Box sx={{ flexGrow: 1 }} className='MobileNav '>
         <AppBar position="static">
         <Toolbar>
-        <Typography variant="p" component="div" sx={{ flexGrow: 1 }} className='text-center text-base'>
+        <Typography variant="p" component="div" sx={{ flexGrow: 1 }} className='text-center text-sm'>
             Endangered Animals You Can Help
           </Typography>
           <FormGroup className='icon'>
